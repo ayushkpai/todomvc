2ee3286f9cec0a95c1ec25de92841d6396a91939
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-07-22 14:50:44 +0530

    inline errors

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,37 +1,28 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i[ edit update destroy ]
 
-  # GET /todos or /todos.json
   def index
     @todo = Todo.new
     @todos = Todo.all
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos or /todos.json
   def create
     @todo = Todo.new(todo_params)
 
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to todos_path, notice: "Todo was successfully created" }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_content }
-        format.json { render json: @todo.errors, status: :unprocessable_content }
-      end
+    if @todo.save
+      redirect_to todos_path, notice: "Todo was successfully created."
+    else
+      redirect_to todos_path, alert: "Todo could not be created."
     end
   end
 
-  # PATCH/PUT /todos/1 or /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
@@ -44,7 +35,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy!
 
@@ -55,12 +45,10 @@ class TodosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def todo_params
       params.expect(todo: [ :name ])
     end
